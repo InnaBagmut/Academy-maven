89beac930f38a86a932fdc51574e308f911d5202
InnaBagmut <energy777&>
2019-11-30 20:30:00 +0200

    lesson 11 & 12 - start hw

--- src/main/java/com/academy/lesson11/homework/Subscribers.java
+++ src/main/java/com/academy/lesson11/homework/Subscribers.java
@@ -1,4 +1,40 @@
 package com.academy.lesson11.homework;
 
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.*;
+
 public class Subscribers {
+    public static void main(String[] args) throws IOException {
+        String pathFrom = "d:\\!!!Autotesting\\!Telesens_Academy\\lesson11\\женские имена.txt";
+        BufferedReader br1 = new BufferedReader(new FileReader(pathFrom));
+        String pathTo = "d:\\!!!Autotesting\\!Telesens_Academy\\lesson11\\subscriber.xlsx";
+        FileOutputStream fos = new FileOutputStream(new File(pathTo));
+        XSSFWorkbook workbook = new XSSFWorkbook();
+
+        XSSFSheet sheet = workbook.createSheet();
+        XSSFRow row = sheet.createRow(0);
+        XSSFCell cell = row.createCell(0);
+
+        br1.lines()
+                .limit(10)
+                .peek(s -> writeNameToExcell(s, sheet))
+                .forEach(System.out::println);
+        workbook.write(fos);
+
+        br1.close();
+        fos.close();
+        workbook.close();
+
+    }
+    public static void writeNameToExcell(String name, XSSFSheet sheet) {
+        int lastRowNum = sheet.getLastRowNum();
+        XSSFRow row = sheet.createRow(lastRowNum + 1);
+        XSSFCell cell = row.createCell(0);
+        cell.setCellValue(name);
+    }
 }
+
--- src/main/java/com/academy/lesson12/JDBCdemo.java
+++ src/main/java/com/academy/lesson12/JDBCdemo.java
@@ -5,20 +5,21 @@
         - добавить в телефонную книгу 5 абонентов, присвоив каждому по номеру
         package com.academy.lesson12;*/
 
+//package com.academy.lesson12;
+
 import java.sql.*;
 
 public class JDBCdemo {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection connection = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/subscriber?user=root&password=root&serverTimezone=UTC");
+                "jdbc:mysql://localhost:3306/subscriber?user=root&password=root&&serverTimezone=UTC");
         Statement statement = connection.createStatement();
 
         //statement.executeUpdate("insert into abonent values(5, 'Smirnov', 'Peter', 'm', 40)");
         //statement.executeUpdate("update abonent set first_name = 'Dream', last_name = 'ComesTrue' where id = 6");
         //statement.executeUpdate("insert into abonent(first_name, last_name, gender, age) values('Lucky', 'Strike', 'm', 44)");
         //statement.executeUpdate("delete from abonent where id = 9");
-
         //String.format("select from {0} where {1} = {}", Table1, col1, id1)
         //String.format("insert into {abonent(first_name, last_name, gender, age)}", "Mike", "Sunday", "m", 32);
 
@@ -37,26 +38,39 @@ public class JDBCdemo {
         statement.executeUpdate("insert into operator_number(number, operator_id) values" +
                 "('380475564823', 4), " +
                 "('380495672004', 5), " +
-                "('380275964123', 6)");*/
-
-        /*statement.executeUpdate("insert into phone_book(number, abonent_id, registered_date) values" +
-                "((select number from operator_number where operator_id = 3), 2, '2019-11-30 14:55:56'), " +
-                "((select number from operator_number where operator_id = 5), 3, '2019-12-30 14:55:56')");*/
+                "('380275964123', 6)");
 
         statement.executeUpdate("insert into phone_book(number, abonent_id, registered_date) values" +
-                "('380475564823', 5, '2019-11-30 14:55:56'), " +
-                "('380495672004', 6, '2019-12-30 14:55:56')");
+                "('380475564823', 17, '2019-11-30 14:55:56'), " +
+                "('380495672004', 18, '2019-12-30 14:55:56')");*/
+
+       /* statement.executeUpdate("insert into phone_book(number, abonent_id, registered_date) values" +
+                "((select number from operator_number where operator_id = 17), 2, '2019-11-30 14:55:56'), " +
+                "((select number from operator_number where operator_id = 18), 3, '2019-12-30 14:55:56')");*/
 
 
-
-
-        ResultSet rs = statement.executeQuery("SELECT * FROM abonent");
-        while (rs.next()) { // указатель на первую строку данных
-            int id = rs.getInt("id");
-            String firstName = rs.getString("first_name");
+        ResultSet rs1 = statement.executeQuery("SELECT * FROM abonent");
+        while (rs1.next()) { // указатель на первую строку данных
+            int id = rs1.getInt("id");
+            String firstName = rs1.getString("first_name");
             System.out.println("id: " + id + ", firstName: " + firstName);
         }
-        rs.close();
+
+        System.out.println("========================================================");
+
+        System.out.println("Выводим всех абонентов заданного мобильного оператора:");
+        ResultSet rs2 = statement.executeQuery("SELECT first_name, last_name FROM abonent where id in" +
+                "(select abonent_id from phone_book where number in" +
+                "(select number from operator_number where operator_id in" +
+                "(select id from operator where name = 'Life')))");
+        while (rs2.next()) {
+            String firstName = rs2.getString("first_name");
+            String lastName = rs2.getString("last_name");
+            System.out.println("Life user: " + " first name: " + firstName + ", last name: " + lastName);
+        }
+
+        rs1.close();
+        rs2.close();
         connection.close();
     }
 }
