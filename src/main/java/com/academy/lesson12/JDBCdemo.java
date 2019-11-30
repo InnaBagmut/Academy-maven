e1e001d62268e04d19652abd15e0c1f6b9911d05
Inna <innasamus@gmail.com>
2019-11-30 15:03:42 +0200

    lesson 12 - homework - starting

--- src/main/java/com/academy/lesson12/JDBCdemo.java
+++ src/main/java/com/academy/lesson12/JDBCdemo.java
@@ -1,4 +1,9 @@
-package com.academy.lesson12;
+/*Добавить в БД Mobile используя sql запросы:
+        - 3 мобильных оператора
+        - 5 абонентов
+        - 9 мобильных номеров (3 номера для каждого оператора)
+        - добавить в телефонную книгу 5 абонентов, присвоив каждому по номеру
+        package com.academy.lesson12;*/
 
 import java.sql.*;
 
@@ -9,14 +14,42 @@ public class JDBCdemo {
                 "jdbc:mysql://localhost:3306/subscriber?user=root&password=root&serverTimezone=UTC");
         Statement statement = connection.createStatement();
 
-/*        statement.executeUpdate("insert into abonent values(5, 'Smirnov', 'Peter', 'm', 40)");
-        statement.executeUpdate("insert into abonent values(6, 'Marfa', 'White', 'f', 36)");
-        statement.executeUpdate("insert into abonent values(7, 'David', 'Blame', 'm', 77)");
-        statement.executeUpdate("insert into abonent values(8, 'Nina', 'Turn', 'f', 8)");
-        statement.executeUpdate("insert into abonent values(9, 'Helen', 'Fri', 'f', 21)");
-        statement.executeUpdate("insert into abonent values(10, 'Ben', 'Raddisson', 'f', 18)");*/
-        statement.executeUpdate("update abonent set first_name = 'Dream', last_name = 'ComesTrue' where id = 6");
-        statement.executeUpdate("delete from abonent where id = 9");
+        //statement.executeUpdate("insert into abonent values(5, 'Smirnov', 'Peter', 'm', 40)");
+        //statement.executeUpdate("update abonent set first_name = 'Dream', last_name = 'ComesTrue' where id = 6");
+        //statement.executeUpdate("insert into abonent(first_name, last_name, gender, age) values('Lucky', 'Strike', 'm', 44)");
+        //statement.executeUpdate("delete from abonent where id = 9");
+
+        //String.format("select from {0} where {1} = {}", Table1, col1, id1)
+        //String.format("insert into {abonent(first_name, last_name, gender, age)}", "Mike", "Sunday", "m", 32);
+
+/*        statement.executeUpdate("insert into operator(id, name, address, fee) values" +
+                "(4, 'BeeLine', 'Haharina avenue 1', 23), " +
+                "(5, 'DreamNet', 'Moskow avenue 217', 56), " +
+                "(6, 'KharkivSun', 'Nauki avenue 14', 24)");
+
+        statement.executeUpdate("insert into abonent(first_name, last_name, gender, age) values" +
+                "('Denny', 'Width', 'm', 43), " +
+                "('Kate', 'Burned', 'f', 22), " +
+                "('Flora', 'Flower', 'f', 32), " +
+                "('Mary', 'Kay', 'f', 65), " +
+                "('Kay', 'Flint', 'f', 43)");
+
+        statement.executeUpdate("insert into operator_number(number, operator_id) values" +
+                "('380475564823', 4), " +
+                "('380495672004', 5), " +
+                "('380275964123', 6)");*/
+
+        /*statement.executeUpdate("insert into phone_book(number, abonent_id, registered_date) values" +
+                "((select number from operator_number where operator_id = 3), 2, '2019-11-30 14:55:56'), " +
+                "((select number from operator_number where operator_id = 5), 3, '2019-12-30 14:55:56')");*/
+
+        statement.executeUpdate("insert into phone_book(number, abonent_id, registered_date) values" +
+                "('380475564823', 5, '2019-11-30 14:55:56'), " +
+                "('380495672004', 6, '2019-12-30 14:55:56')");
+
+
+
+
         ResultSet rs = statement.executeQuery("SELECT * FROM abonent");
         while (rs.next()) { // указатель на первую строку данных
             int id = rs.getInt("id");
