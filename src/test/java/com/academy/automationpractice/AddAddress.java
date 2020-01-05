1c0e85b4c6085c3b53eb5266ecc5f756ea3a683a
Inna <innasamus@gmail.com>
2020-01-05 22:19:17 +0200

    lesson 19 - hw

--- src/test/java/com/academy/automationpractice/AddAddress.java
+++ src/test/java/com/academy/automationpractice/AddAddress.java
@@ -5,8 +5,15 @@ import com.academy.automationpractice.page.LoginPage;
 import com.academy.automationpractice.page.MyAddressPage;
 import com.academy.core.BaseTest;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class AddAddress extends BaseTest {
@@ -14,8 +21,10 @@ public class AddAddress extends BaseTest {
     public void onException(Throwable err, WebDriver driver) {
     }
 
-    @Test
-    public void addAddressTest() {
+    @Test(dataProvider = "addAddressDataProvider")
+    public void addAddressTest(String firstName, String lastName, String company, String addressMain,
+                               String addressAdditional, String city, String zipCode, String homePhone,
+                               String mobilePhone, String additionalInfo, String addressTitle) {
 
         LoginPage myObject1 = new HomePage(driver, baseUrl)
                 .goToHome()
@@ -27,6 +36,18 @@ public class AddAddress extends BaseTest {
         MyAddressPage myObject2 = new MyAddressPage(driver)
                 .clickOnMyAddresses()
                 .clickAddAddress()
+                .fillFirstName(firstName)
+                .fillLastName(lastName)
+                .fillCompany(company)
+                .chooseState()
+                .fillAddressMain(addressMain)
+                .fillAddressAdditional(addressAdditional)
+                .fillCity(city)
+                .fillZipCode(zipCode)
+                .fillHomePhone(homePhone)
+                .fillMobilePhone(mobilePhone)
+                .fillAdditionalInfo(additionalInfo)
+                .fillAddressTitle(addressTitle)
                 .clickOnSubmit();
 
         BaseTest myObject3 = new BaseTest() {
@@ -37,4 +58,25 @@ public class AddAddress extends BaseTest {
         // myObject3.clickOnElement();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     }
+
+    @DataProvider(name = "addAddressDataProvider")
+    public Object[][] addAddressDataProvider() throws IOException {
+        String file = properties.getProperty("data.dir") + "/addAddressDataWithoutStateAndCountry.csv";
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
+        List<String> data = new ArrayList<>();
+        String line;
+        while ((line = br.readLine()) != null)
+            if(!line.trim().startsWith("#"))
+                data.add(line);
+            br.close();
+            fr.close();
+
+            Object[][] result = new Object[data.size()][12];
+        for (int i = 0; i < data.size(); i++) {
+            result[i] = data.get(i).split(",");
+        }
+            return result;
+    }
+
 }
--- src/test/java/com/academy/automationpractice/page/MyAddressPage.java
+++ src/test/java/com/academy/automationpractice/page/MyAddressPage.java
@@ -1,9 +1,17 @@
 package com.academy.automationpractice.page;
 
+import com.academy.automationpractice.MyAddress;
 import com.academy.core.BasePage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.beans.PropertyChangeListener;
 
 public class MyAddressPage extends BasePage {
     @FindBy(css = "#center_column > div > div:nth-child(1) > ul > li:nth-child(3) > a > span")
@@ -15,11 +23,50 @@ public class MyAddressPage extends BasePage {
     @FindBy(css = "#center_column > div.clearfix.main-page-indent > a > span")
     private WebElement addAddressBtn;
 
+    @FindBy(id = "firstname")
+    private WebElement firstNameInput;
+
+    @FindBy(id = "lastname")
+    private WebElement lastNameInput;
+
     @FindBy(id = "company")
-    private WebElement companyField;
+    private WebElement companyInput;
+
+    @FindBy(id = "address1")
+    private WebElement addressMainInput;
+
+    @FindBy(id = "address2")
+    private WebElement addressAdditionalInput;
+
+    @FindBy(id = "city")
+    private WebElement cityInput;
+
+    @FindBy(id = "id_state")
+    private WebElement stateInput;
+
+    @FindBy(id = "postcode")
+    private WebElement zipCodeInput;
+
+    @FindBy(id = "id_country")
+    private WebElement countryInput;
+
+    @FindBy(id = "phone")
+    private WebElement homePhoneInput;
+
+    @FindBy(id = "phone_mobile")
+    private WebElement mobilePhoneInput;
+
+    @FindBy(id = "other")
+    private WebElement additionalInfoInput;
+
+    @FindBy(id = "alias")
+    private WebElement addressTitleInput;
 
     @FindBy(id = "submitAddress")
     private WebElement submitBtn;
+
+    Actions actions = new Actions(driver);
+
 
     public MyAddressPage(WebDriver driver) {
         super(driver);
@@ -35,8 +82,73 @@ public class MyAddressPage extends BasePage {
         return this;
     }
 
-    public MyAddressPage fillCompany() {
-        fillInput(companyField, "Fairytale");
+    public MyAddressPage fillFirstName(String firstName) {
+        fillInput(firstNameInput, firstName);
+        return this;
+    }
+
+    public MyAddressPage fillLastName(String lastName) {
+        fillInput(lastNameInput, lastName);
+        return this;
+    }
+
+    public MyAddressPage fillCompany(String company) {
+        fillInput(companyInput, company);
+        return this;
+    }
+
+    public MyAddressPage fillAddressMain(String addressMain) {
+        fillInput(addressMainInput, addressMain);
+        return this;
+    }
+
+    public MyAddressPage fillAddressAdditional(String addressAdditional) {
+        fillInput(addressAdditionalInput, addressAdditional);
+        return this;
+    }
+
+    public MyAddressPage fillCity(String city) {
+        fillInput(cityInput, city);
+        return this;
+    }
+
+    public MyAddressPage chooseState() {
+/*        stateInput.click();
+        actions.keyDown("H")
+                .perform();*/
+        Select dropDown = new Select(stateInput);
+        dropDown.selectByVisibleText("Colorado");
+        return this;
+    }
+
+    public MyAddressPage fillZipCode(String zipCode) {
+        fillInput(zipCodeInput, zipCode);
+        return this;
+    }
+
+    public MyAddressPage chooseCountry() {
+        Select dropDown = new Select(countryInput);
+        dropDown.selectByVisibleText("United States");
+        return this;
+    }
+
+    public MyAddressPage fillHomePhone(String homePhone) {
+        fillInput(homePhoneInput, homePhone);
+        return this;
+    }
+
+    public MyAddressPage fillMobilePhone(String mobilePhone) {
+        fillInput(mobilePhoneInput, mobilePhone);
+        return this;
+    }
+
+    public MyAddressPage fillAdditionalInfo(String additionalInfo) {
+        fillInput(addressAdditionalInput, additionalInfo);
+        return this;
+    }
+
+    public MyAddressPage fillAddressTitle(String addressTitle) {
+        fillInput(addressTitleInput, addressTitle);
         return this;
     }
 
